ad3e59b220ec94958698d9baa2d02dbef3720659
ivencomur <ivancortes@hotmail.com>
2025-08-14 18:00:11 -0400

    Revert "Reapply "Update App.js to use secure Firebase config""
    This reverts commit 79b75818b0c6a13325671b9d96652f32a1636c6f.

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from './firebase-config';
 import Start from './components/Start';
 import Chat from './components/Chat';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen
-          name="Start"
-          component={Start}
+        <Stack.Screen 
+          name="Start" 
+          component={Start} 
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Chat">
-          {props => <Chat db={db} {...props} />}
-        </Stack.Screen>
+        <Stack.Screen name="Chat" component={Chat} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
-
-export default App;
+}
