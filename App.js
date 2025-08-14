4f837f28b13aa1379f7e3b5b24d59a3c61cfd6bd
ivencomur <ivancortes@hotmail.com>
2025-08-14 19:05:17 -0400

    Update App.js to use secure Firebase config

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { firebaseConfig } from './firebase-config';
 import Start from './components/Start';
 import Chat from './components/Chat';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+const App = () => {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen 
-          name="Start" 
-          component={Start} 
+        <Stack.Screen
+          name="Start"
+          component={Start}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat">
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
